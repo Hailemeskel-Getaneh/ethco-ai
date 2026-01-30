b110a7d82c2c292963ff2a3e8f0e5f241e078e2e
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-01-30 19:30:00 +0300

    docs(mobile): add JSDoc to Icon component

--- apps/mobile/src/components/ui/Icons.tsx
+++ apps/mobile/src/components/ui/Icons.tsx
@@ -3,6 +3,14 @@ import { colors } from '@/theme/colors';
 
 // Wrapper for consistency and easier refactoring
 // Using Ionicons as the standard set
+/**
+ * Icon component wrapper around Ionicons.
+ * 
+ * @param {object} props - The props for the component.
+ * @param {keyof typeof Ionicons.glyphMap} props.name - The name of the icon.
+ * @param {number} [props.size=24] - The size of the icon.
+ * @param {string} [props.color=colors.text] - The color of the icon.
+ */
 export const Icon = ({
     name,
     size = 24,
