fc735f2cf6e260cfbb066e698a82ba494c6a691f
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-06 11:20:00 +0300

    feat(mobile): add theme index for centralized theme exports

---
+++ apps/mobile/src/theme/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Theme index file for exporting all theme constants.
+ * Provides a centralized import point for theme-related utilities.
+ */
+
+export { colors } from './colors';
+export { typography } from './typography';
+export { spacing } from './spacing';
+export { borderRadius } from './borderRadius';
